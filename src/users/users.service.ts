4a2021a4ad288f19fb1ae81115bb103e2f9478b8
Hetor17 <hemr17@hotmail.com>
2025-06-19 15:52:24 -0700

    Little Changes

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,26 @@ import { CreateUserDto } from './dto/create-user-dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
+/**
+ * Service responsible for managing user-related operations.
+ * 
+ * The `UsersService` class provides methods to interact with the user data in the database.
+ * It leverages the injected `PrismaService` to perform database operations such as retrieving all users
+ * and creating new users.
+ * 
+ * - The `getUsers` method fetches and returns all user records from the database.
+ * - The `createUser` method creates a new user record using the provided `CreateUserDto` data transfer object.
+ * 
+ * This service acts as an abstraction layer between the controller and the database, ensuring that
+ * all user-related data access logic is centralized and reusable.
+ */
 export class UsersService {
   
   // Injects the PrismaService to interact with the database  npm install --legacy-peer-deps
   constructor(private prisma: PrismaService) {}
 
   // Returns all users from the database
+  
   getUsers() {
     return this.prisma.user.findMany();
   }
